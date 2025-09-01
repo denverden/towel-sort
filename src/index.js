c2634b0f617ca0263a32b04747de7f6141cfa25f
DenverDen <denverden@mail.ru>
2025-09-01 13:09:49 +0300

    feat: add solution towelsort

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (typeof matrix === 'undefined') return [];
+  let result = [];
+  for (let i = 0; i < matrix.length; i += 1) {
+    if (i % 2 === 0) result = result.concat(matrix[i]);
+    else result = result.concat(matrix[i].reverse());
+  }
+  return result;
 };
